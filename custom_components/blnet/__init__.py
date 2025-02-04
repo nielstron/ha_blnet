57efb818575a598f9c0e23b1b0ef58241f7eb6b8
aignermax <max_aigneraigner@web.de>
2025-02-04 23:34:33 +0100

    test - changed the code to adhere to the solid design principles so that it could be easier maintained.

--- custom_components/blnet/__init__.py
+++ custom_components/blnet/__init__.py
@@ -2,50 +2,46 @@
 Connect to a BL-NET via it's web interface and read and write data
 """
 import logging
+from datetime import datetime, timedelta
 
 import voluptuous as vol
-from homeassistant.helpers.discovery import load_platform
-
 from homeassistant.const import (
     CONF_RESOURCE, CONF_PASSWORD, CONF_SCAN_INTERVAL, UnitOfTemperature,
 )
+from homeassistant.helpers.discovery import load_platform
 from homeassistant.helpers.event import async_track_time_interval
-from datetime import timedelta
-from datetime import datetime
-import time
 import homeassistant.helpers.config_validation as cv
 
-REQUIREMENTS = [
-    'pyblnet==0.9.3'
-]
+REQUIREMENTS = ['pyblnet==0.9.3']
 
 _LOGGER = logging.getLogger(__name__)
-
 DOMAIN = 'blnet'
 
+# Configuration constants
 CONF_WEB_PORT = 'web_port'
 CONF_TA_PORT = 'ta_port'
 CONF_USE_WEB = 'use_web'
 CONF_USE_TA = 'use_ta'
 CONF_NODE = 'can_node'
+
 # Defaults
 DEFAULT_WEB_PORT = 80
 DEFAULT_TA_PORT = 40000
-# scan every 6 minutes per default
 DEFAULT_SCAN_INTERVAL = 360
 
-UNIT = {
+# Unit and icon mappings
+UNIT_MAPPINGS = {
     'analog': UnitOfTemperature.CELSIUS,
     'speed': 'rpm',
     'power': 'kW',
     'energy': 'kWh'
 }
-ICON = {
+
+ICON_MAPPINGS = {
     'analog': 'mdi:thermometer',
     'speed': 'mdi:speedometer',
     'power': 'mdi:power-plug',
     'energy': 'mdi:power-plug'
-
 }
 
 CONFIG_SCHEMA = vol.Schema({
@@ -53,8 +49,7 @@ CONFIG_SCHEMA = vol.Schema({
         vol.Required(CONF_RESOURCE): cv.url,
         vol.Optional(CONF_PASSWORD): cv.string,
         vol.Optional(CONF_NODE): cv.positive_int,
-        vol.Optional(CONF_SCAN_INTERVAL,
-                     default=DEFAULT_SCAN_INTERVAL): cv.positive_int,
+        vol.Optional(CONF_SCAN_INTERVAL, default=DEFAULT_SCAN_INTERVAL): cv.positive_int,
         vol.Optional(CONF_WEB_PORT, default=DEFAULT_WEB_PORT): cv.positive_int,
         vol.Optional(CONF_TA_PORT, default=DEFAULT_TA_PORT): cv.positive_int,
         vol.Optional(CONF_USE_WEB, default=True): cv.boolean,
@@ -65,161 +60,191 @@ CONFIG_SCHEMA = vol.Schema({
 
 def setup(hass, config):
     """Set up the BLNET component"""
-
     from pyblnet import BLNET
 
-    config = config[DOMAIN]
-    resource = config.get(CONF_RESOURCE)
-    password = config.get(CONF_PASSWORD)
-    can_node = config.get(CONF_NODE)
-    scan_interval = config.get(CONF_SCAN_INTERVAL)
-    web_port = config.get(CONF_WEB_PORT)
-    ta_port = config.get(CONF_TA_PORT)
-    use_web = config.get(CONF_USE_WEB)
-    use_ta = config.get(CONF_USE_TA)
-
-    # Initialize the BL-NET sensor
+    conf = config[DOMAIN]
+    blnet_connector = BLNETConnector(conf, BLNET)
+    
     try:
-        blnet = BLNET(resource, password=password, web_port=web_port,
-                      ta_port=ta_port, use_web=use_web, use_ta=use_ta)
+        blnet = blnet_connector.connect()
     except (ValueError, AssertionError) as ex:
-        if isinstance(ex, ValueError):
-            _LOGGER.error("No BL-Net reached at {}".format(resource))
-        else:
-            _LOGGER.error("Configuration invalid: {}".format(ex))
+        _LOGGER.error(blnet_connector.get_error_message(ex, conf[CONF_RESOURCE]))
         return False
 
+    # Initialize the data handler
+    data_handler = BLNETDataHandler(blnet, conf.get(CONF_NODE), hass, conf)
+    hass.data[f"DATA_{DOMAIN}"] = data_handler
 
-    # set the communication entity
-    hass.data["DATA_{}".format(DOMAIN)] = BLNETComm(blnet, can_node, hass, config)
+    # Set up periodic updates
+    update_handler = BLNETUpdateHandler(hass, data_handler, conf[CONF_SCAN_INTERVAL])
+    update_handler.schedule_updates()
 
-    # make sure the communication device gets updated once in a while
-    def fetch_data(*_):
-        return hass.data["DATA_{}".format(DOMAIN)].update()
-
-    # Get the latest data from REST API and load
-    # sensors and switches accordingly
-    data = fetch_data()
-    # Repeat if data fetching fails at first
-    async_track_time_interval(hass,
-                              fetch_data,
-                              timedelta(seconds=scan_interval))
-
-    # Fetch method takes care of adding dicovered sensors
     return True
 
 
-class BLNETComm(object):
-    """Implementation of a BL-NET - UVR1611 communication component"""
+class BLNETConnector:
+    """Handles connection to BLNET device."""
+    
+    def __init__(self, config, blnet_class):
+        self.config = config
+        self.BLNET = blnet_class
+
+    def connect(self):
+        """Create and return BLNET connection."""
+        return self.BLNET(
+            self.config[CONF_RESOURCE],
+            password=self.config.get(CONF_PASSWORD),
+            web_port=self.config[CONF_WEB_PORT],
+            ta_port=self.config[CONF_TA_PORT],
+            use_web=self.config[CONF_USE_WEB],
+            use_ta=self.config[CONF_USE_TA]
+        )
+
+    def get_error_message(self, exception, resource):
+        """Generate appropriate error message."""
+        if isinstance(exception, ValueError):
+            return f"No BL-Net reached at {resource}"
+        return f"Configuration invalid: {exception}"
+
+
+class BLNETDataHandler:
+    """Handles data operations for BLNET."""
 
     def __init__(self, blnet, node, hass, config):
         self.blnet = blnet
         self.node = node
-        # Map id -> attributes
         self.data = {}
         self._last_updated = None
         self._hass = hass
         self._config = config
         self.sensors = set()
 
-    def turn_on(self, switch_id):
-        # only change active node if this is desired
-        self.blnet.turn_on(switch_id, self.node)
-
-    def turn_off(self, switch_id):
-        # only change active node if this is desired
-        self.blnet.turn_off(switch_id, self.node)
-
-    def turn_auto(self, switch_id):
-        # only change active node if this is desired
-        self.blnet.turn_auto(switch_id, self.node)
-
-    def last_updated(self):
-        return self._last_updated
-
     def update(self):
-        """
-        Get the latest data from BLNET and update the state.
-        Also adds new states on discovery.
-        """
+        """Update all data and handle sensor discovery."""
+        data = self._fetch_data()
+        self._update_sensor_data(data)
+        self._discover_new_devices(data)
+        return data
 
-        data = self.blnet.fetch(self.node)
+    def _fetch_data(self):
+        """Fetch raw data from BLNET device."""
+        return self.blnet.fetch(self.node)
 
-        _LOGGER.info("Collecting data...")
-        for domain in ['analog', 'speed', 'power', 'energy']:
-            # iterate through the list and store the data for every sensor
-            for key, sensor in data.get(domain, {}).items():
-                attributes = {}
-                entity_id = '{} {} {}'.format(DOMAIN, domain, key)
-                attributes['value'] = sensor.get('value')
-
-                attributes['unit_of_measurement'] = sensor.get('unit_of_measurement',
-                                                               UNIT[domain])
-                attributes['friendly_name'] = sensor.get('name')
-                attributes['icon'] = ICON[domain]
-
-                self.data[entity_id] = attributes
-
-        # iterate through the list and store data for every output
-        for key, sensor in data.get('digital', {}).items():
-            attributes = {}
-            entity_id = '{} digital {}'.format(DOMAIN, key)
-
-            attributes['friendly_name'] = sensor.get('name')
-            attributes['mode'] = sensor.get('mode')
-            attributes['value'] = sensor.get('value')
-
-            self.data[entity_id] = attributes
-        # save that the data was updated just now
+    def _update_sensor_data(self, data):
+        """Update data for existing sensors."""
+        _LOGGER.info("Updating sensor data...")
+        self._update_domain_sensors(data)
+        self._update_digital_sensors(data)
         self._last_updated = datetime.now()
 
-        _LOGGER.info("Checking for new sensors...")
-        # Check if there are new sensors that are to be added
-        i = 0
-        # iterate through the list and create a sensor for every value
+    def _update_domain_sensors(self, data):
+        """Update sensors for all domains."""
+        for domain in ['analog', 'speed', 'power', 'energy']:
+            for key, sensor in data.get(domain, {}).items():
+                self._update_single_sensor(domain, key, sensor)
+
+    def _update_single_sensor(self, domain, key, sensor):
+        """Update a single sensor's attributes."""
+        entity_id = f'{DOMAIN} {domain} {key}'
+        self.data[entity_id] = {
+            'value': sensor.get('value'),
+            'unit_of_measurement': sensor.get('unit_of_measurement', UNIT_MAPPINGS[domain]),
+            'friendly_name': sensor.get('name'),
+            'icon': ICON_MAPPINGS[domain]
+        }
+
+    def _update_digital_sensors(self, data):
+        """Update digital sensors."""
+        for key, sensor in data.get('digital', {}).items():
+            entity_id = f'{DOMAIN} digital {key}'
+            self.data[entity_id] = {
+                'friendly_name': sensor.get('name'),
+                'mode': sensor.get('mode'),
+                'value': sensor.get('value')
+            }
+
+    def _discover_new_devices(self, data):
+        """Handle discovery of new devices."""
+        added_count = self._discover_sensors(data)
+        added_count += self._discover_digital_devices(data)
+        if added_count > 0:
+            _LOGGER.info(f"Added {added_count} new devices")
+
+    def _discover_sensors(self, data):
+        """Discover and add new sensors."""
+        added_count = 0
         for domain in ['analog', 'speed', 'power', 'energy']:
             for sensor_id in data[domain]:
-                name = data[domain][sensor_id].get('name') # 
-                blnet_id = '{} {} {}'.format(DOMAIN, domain, sensor_id)
-                if name in self.sensors:
-                    continue
-                self.sensors.add(name)
-                _LOGGER.info("Discovered {} sensor {} in use, adding".format(domain, sensor_id))
-                
-                i += 1
+                if self._add_single_sensor(domain, sensor_id, data):
+                    added_count += 1
+        return added_count
 
-                disc_info = {
-                    'name': name,
-                    'blnet_id': blnet_id,
-                    'domain': domain,
-                    'id': sensor_id,
-                    'friendly_name': data[domain][sensor_id].get('name')
-                }
-                load_platform(self._hass, 'sensor', DOMAIN, disc_info, self._config)
+    def _add_single_sensor(self, domain, sensor_id, data):
+        """Add a single new sensor."""
+        name = data[domain][sensor_id].get('name')
+        if name in self.sensors:
+            return False
 
-                _LOGGER.debug(f"Sensor data for {domain}[{sensor_id}]: {data[domain][sensor_id]} - Disc info: {disc_info}")
+        self.sensors.add(name)
+        _LOGGER.info(f"Discovered {domain} sensor {sensor_id} in use, adding")
 
-        # iterate through the list and create a switch for direct control over every output
+        disc_info = {
+            'name': name,
+            'domain': domain,
+            'id': sensor_id,
+            'friendly_name': name
+        }
+        _LOGGER.debug(f"Sensor data for {domain}[{sensor_id}]: {data[domain][sensor_id]} - Disc info: {disc_info}")
+        load_platform(self._hass, 'sensor', DOMAIN, disc_info, self._config)
+        return True
+
+    def _discover_digital_devices(self, data):
+        """Discover and add new digital devices."""
+        added_count = 0
         for sensor_id in data['digital']:
-            name = data['digital'][sensor_id].get('name') #'{} digital {}'.format(DOMAIN, sensor_id)
-            if name in self.sensors:
-                continue
-            self.sensors.add(name)
-            _LOGGER.info("Discovered digital sensor {} in use, adding".format(sensor_id))
-            i += 1
-            disc_info = {
-                'name': name,
-                'blnet_id': blnet_id,
-                'domain': 'digital',
-                'id': sensor_id,
-                'friendly_name': data['digital'][sensor_id].get('name')
-            }
-            if self._config.get(CONF_USE_WEB):
-                component = 'switch'
-            else:
-                component = 'sensor'
-            load_platform(self._hass, component, DOMAIN, disc_info, self._config)
-        if i > 0:
-            _LOGGER.info("Added overall {} sensors".format(i))
+            if self._add_digital_device(sensor_id, data):
+                added_count += 1
+        return added_count
 
-        return data
+    def _add_digital_device(self, sensor_id, data):
+        """Add a single new digital device."""
+        name = data['digital'][sensor_id].get('name')
+        if name in self.sensors:
+            return False
+
+        self.sensors.add(name)
+        _LOGGER.info(f"Discovered digital sensor {sensor_id} in use, adding")
+
+        disc_info = {
+            'name': name,
+            'domain': 'digital',
+            'id': sensor_id,
+            'friendly_name': name
+        }
+        component = 'switch' if self._config[CONF_USE_WEB] else 'sensor'
+        load_platform(self._hass, component, DOMAIN, disc_info, self._config)
+        return True
+
+
+class BLNETUpdateHandler:
+    """Handles periodic updates for BLNET."""
+
+    def __init__(self, hass, data_handler, scan_interval):
+        self.hass = hass
+        self.data_handler = data_handler
+        self.scan_interval = scan_interval
+
+    def schedule_updates(self):
+        """Schedule periodic updates."""
+        def fetch_data(*_):
+            return self.data_handler.update()
+
+        # Initial update
+        fetch_data()
+        
+        # Schedule periodic updates
+        async_track_time_interval(
+            self.hass,
+            fetch_data,
+            timedelta(seconds=self.scan_interval)
+        )
